b1f3ca4db2fdeba779031a0becefac173ef6d958
Rogério Bayer <tomatebg@gmail.com>
2021-07-22 00:51:46 -0300

    [wip] ClienteController

---
+++ src/pizzaria/controller/ClienteController.java
@@ -0,0 +1,74 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pizzaria.controller;
+
+import java.util.List;
+import pizzaria.model.Cliente;
+import pizzaria.model.dao.ClienteDao;
+import pizzaria.view.JanelaClienteView;
+
+public class ClienteController {
+    private JanelaClienteView view;
+    private ClienteDao modelDao;
+
+    public ClienteController(JanelaClienteView view, ClienteDao modelDao) {
+        this.view = view;
+        this.modelDao = modelDao;
+        initController();
+    }
+    
+    private void initController(){
+        this.view.setController(this);
+        this.view.initView();
+    }
+
+    public void criarCliente() {
+        try{
+            Cliente cliente = view.getClienteFormulario();
+            modelDao.inserir(cliente);
+            view.inserirClienteView(cliente);
+        }catch(Exception ex){
+            view.apresentaErro("Erro ao criar cliente.");
+        }
+    }
+
+    public void atualizarCliente() {
+        try{
+            
+            Cliente cliente = view.getClienteParaAtualizar();
+            if(cliente==null){
+                view.apresentaInfo("Selecione um cliente na tabela para atualizar.");
+                return;
+            }
+            modelDao.atualizar(cliente);
+            view.atualizarCliente(cliente);
+            
+        }catch(Exception ex){
+            view.apresentaErro("Erro ao atualizar cliente.");
+        }
+    }
+
+    public void excluirCliente() {
+        try{
+            List<Cliente> listaParaExcluir = view.getClientesParaExcluir();
+            modelDao.exluirLista(listaParaExcluir);
+            view.excluirClientesView(listaParaExcluir);
+        }catch(Exception ex){
+            view.apresentaErro("Erro ao excluir clientes.");
+        }
+    }
+
+    public void listarCliente() {
+        try{
+            List<Cliente> lista = this.modelDao.getLista();
+            view.mostrarListaClientes(lista);
+        }catch(Exception ex){
+            view.apresentaErro("Erro ao listar clientes.");
+        }
+    }
+    
+    
+}
