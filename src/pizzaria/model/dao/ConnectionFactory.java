cefcd9d4ca4169cf671c57971a5fb933efa9d4a6
Rogério Bayer <tomatebg@gmail.com>
2021-08-05 12:37:00 -0300

    [fix] sql connection

--- src/pizzaria/model/dao/ConnectionFactory.java
+++ src/pizzaria/model/dao/ConnectionFactory.java
@@ -8,7 +8,7 @@ public class ConnectionFactory {
 
     public Connection getConnection() {
         try {
-            return DriverManager.getConnection("jdbc:mysql://localhost:3306/sales?allowPublicKeyRetrieval=true&useTimezone=true&serverTimezone=UTC&useSSL=false", "root", "123risi");//"rrw@and14"
+            return DriverManager.getConnection("jdbc:mysql://localhost:3306/pizzaria?allowPublicKeyRetrieval=true&useTimezone=true&serverTimezone=UTC&useSSL=false", "root", "123risi");//"rrw@and14"
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
